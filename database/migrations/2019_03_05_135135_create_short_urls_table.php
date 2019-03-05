c38a54c6e174c8090b7509275193ac38e8376780
Miguel Angel Hernandez Cruz <mhernandez@unphu.edu.do>
2019-03-05 11:02:16 -0400

    termination of migrations for the creation of tables and their relationships

---
+++ database/migrations/2019_03_05_135135_create_short_urls_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShortUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('short_urls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('url');
+            $table->text('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('short_urls');
+    }
+}
