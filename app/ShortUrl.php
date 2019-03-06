8d606a703de81f0c22e6ffc2e1675250d3c258b4
Miguel Angel Hernandez Cruz <mhernandez@unphu.edu.do>
2019-03-06 01:49:21 -0400

    Generation of new code and validation before creating a new short url

--- app/ShortUrl.php
+++ app/ShortUrl.php
@@ -2,9 +2,39 @@
 
 namespace App;
 
+use App\utils\UrlShort;
 use Illuminate\Database\Eloquent\Model;
+
 
 class ShortUrl extends Model
 {
     protected $fillable = ['url', 'code', 'counter'];
+
+    /**
+     * @return mixed
+     */
+
+    public  function  validateShortUrl(string $validUrl) {
+
+        $valid = ShortUrl::where('code', $validUrl)->count() ? false : true;
+
+        return $valid;
+    }
+
+
+    public function generateShortUrl() {
+        $urlShort = new UrlShort();
+
+        for ($i=0; $i < 100 ; $i++) {
+            $newCode = $urlShort->generate();
+            $valid = $this->validateShortUrl($newCode);
+            if ($valid) {
+                $this->code = $newCode;
+            }
+        }
+
+        return $this->code;
+    }
+
 }
+
